9bff9932c7b08a30a32db3a0dda63630d6c7310f
KentaMorikawa <morikawa.kenta.mystery@gmail.com>
2021-07-28 10:18:26 +0900

    urls.pyのresultのパスを変更

--- calc_four_factors/urls.py
+++ calc_four_factors/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns=[
     path("",views.index,name="index"),
-    path("calc_four_factors/result",views.result,name="result"),
+    path("result",views.result,name="result"),
     #path("plot/",views.get_svg,name="plot")
 ]
