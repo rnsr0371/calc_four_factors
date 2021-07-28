83fa953110a87c15f5ef0cbf659937be7edbcdb7
KentaMorikawa <morikawa.kenta.mystery@gmail.com>
2021-07-28 09:21:35 +0900

    settings.pyのdj_database_url.configに引数を2つ追加

--- four_factors/settings.py
+++ four_factors/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 
 
 import dj_database_url
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 
