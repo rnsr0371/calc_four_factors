04360a02a4e5e31711756172eec7b590bf9c6870
KentaMorikawa <morikawa.kenta.mystery@gmail.com>
2021-07-28 14:59:50 +0900

    saveする内容にidを明示してみたら通ったので、model.pyのteamでidを明示的に書かないようにした

--- calc_four_factors/models.py
+++ calc_four_factors/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 #teamsテーブル
 class Team(models.Model):
-    id=models.IntegerField(primary_key=True)
     teamname=models.CharField(max_length=100)
 
 #ベーシックスタッツテーブル
