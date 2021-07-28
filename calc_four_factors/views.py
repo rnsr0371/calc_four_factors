af8ff95d2ccf6059ded9ea0a42b5db52dbd6e442
KentaMorikawa <morikawa.kenta.mystery@gmail.com>
2021-07-28 09:49:46 +0900

    エラーを表示する部分を変更（views.pyの最後の部分）

--- calc_four_factors/views.py
+++ calc_four_factors/views.py
@@ -213,7 +213,12 @@ def result(request):
     }
     return render(request,"calc_four_factors/result.html",params)
 
-#@requires_csrf_token
+
+from django.views.decorators.csrf import requires_csrf_token
+from django.http import (
+    HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotFound,
+    HttpResponseServerError,)
+@requires_csrf_token
 def my_customized_server_error(request, template_name='500.html'):
     import sys
     from django.views import debug
